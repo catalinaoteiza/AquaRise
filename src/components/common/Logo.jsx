0452c42b299d2789865955ba92b52003a91be100
Catalina Oteiza <catalinaoteiza7@gmail.com>
2026-08-27 00:46:01 -0400

    Replace navbar logo image asset with icon-only symbol (water drop/leaf/fish/globe, NO text)

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function Logo({ size = 'md', className = '' }) {
   const logoHeights = {
-    sm: 'h-9 sm:h-11',
-    md: 'h-13 sm:h-16',
-    lg: 'h-16 sm:h-20',
-    xl: 'h-20 sm:h-24',
+    sm: 'h-8 sm:h-9',
+    md: 'h-[44px] sm:h-[48px]',
+    lg: 'h-14 sm:h-16',
+    xl: 'h-18 sm:h-20',
   };
 
   return (
     <div className={`inline-flex items-center justify-center shrink-0 group cursor-pointer ${className}`}>
       <img
         src="/aquarise-logo.png"
-        alt="AquaRise"
+        alt="AquaRise Symbol"
         className={`${logoHeights[size]} w-auto object-contain transition-transform duration-300 group-hover:scale-105 block`}
       />
     </div>
