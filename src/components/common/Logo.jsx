019a6fc950ad5a103970567299d185ca88ef5241
Catalina Oteiza <catalinaoteiza7@gmail.com>
2026-08-27 01:05:50 -0400

    Replace navbar logo with attached image asset, preserving symbol and setting AquaRise wordmark to light blue (#38BDF8)

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Logo({ size = 'md', className = '' }) {
   const logoHeights = {
-    sm: 'h-8 sm:h-9',
-    md: 'h-[44px] sm:h-[48px]',
+    sm: 'h-9',
+    md: 'h-11 sm:h-13',
     lg: 'h-14 sm:h-16',
     xl: 'h-18 sm:h-20',
   };
@@ -12,7 +12,7 @@ export default function Logo({ size = 'md', className = '' }) {
     <div className={`inline-flex items-center justify-center shrink-0 group cursor-pointer ${className}`}>
       <img
         src="/aquarise-logo.png"
-        alt="AquaRise Symbol"
+        alt="AquaRise"
         className={`${logoHeights[size]} w-auto object-contain transition-transform duration-300 group-hover:scale-105 block`}
       />
     </div>
